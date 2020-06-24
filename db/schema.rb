54c82c9892305eaaba6c0839f457405881d8e68b
Owner <owner@MA4-0029.local>
2020-06-24 13:51:21 +0900

    bookers2

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_151813) do
+ActiveRecord::Schema.define(version: 2020_06_24_014900) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "profile_image_id"
-  end
-
-  create_table "post_books", force: :cascade do |t|
-    t.text "title"
-    t.text "body"
-    t.integer "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
